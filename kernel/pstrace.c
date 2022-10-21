3341016d35c3eb2408bf59463a72151c98b2cb58
Mohsin Rizvi <mkrizvi98@gmail.com>
2022-10-21 00:53:25 -0400

    enforce checkpatch compliance

--- kernel/pstrace.c
+++ kernel/pstrace.c
@@ -10,20 +10,21 @@
 #include <linux/wait.h>
 
 struct pstrace ring_buf[PSTRACE_BUF_SIZE];
-unsigned long flags = 0;
-DEFINE_SPINLOCK(ring_buf_lock); /* used for locking the ring_buf, ring_buf_len, and traced_pid */
-//spinlock_t ring_buf_lock; 
-int ring_buf_len = 0;  /* index of latest entry in the ring buffer */
-long ring_buf_count = 0; /* number of records added ever */
-int ring_buf_valid_count = 0; /* number of records added since last clear */
-atomic_t clear_count; /* used for conditionally stopping waiting when we clear the buffer */
+unsigned long flags;
+ /* used for locking the ring_buf, ring_buf_len, and traced_pid */
+DEFINE_SPINLOCK(ring_buf_lock);
+int ring_buf_len;  /* index of latest entry in the ring buffer */
+long ring_buf_count; /* number of records added ever */
+int ring_buf_valid_count; /* number of records added since last clear */
+/* used for conditionally stopping waiting when we clear the buffer */
+atomic_t clear_count;
 pid_t traced_pid = -2; /* the pid we are tracing, or -1 for all processes,
 			* or -2 for tracing disabled
 			*/
 DECLARE_WAIT_QUEUE_HEAD(wq_head);
-bool is_wakeup_required = false;
-long linux_counter = 0;
-int orig_clear_count = 0;
+bool is_wakeup_required;
+long linux_counter;
+int orig_clear_count;
 struct mutex pstrace_mutex; /* used for locking ring_buf and sleep */
 pid_t syscall443_pid = -10;
 
@@ -38,7 +39,6 @@ void insert_pstrace_entry(struct task_struct *p, long state)
 	ring_buf[ring_buf_len].state = state;
 	ring_buf[ring_buf_len].pid = p->tgid;
 	ring_buf[ring_buf_len].tid = p->pid;
-	printk("Insert Ring_buff[%d] info:\ncomm: %s\nstate: %ld\n", ring_buf_len, ring_buf[ring_buf_len].comm, ring_buf[ring_buf_len].state);
 	ring_buf_count++;
 
 	// wake_up?
@@ -46,7 +46,6 @@ void insert_pstrace_entry(struct task_struct *p, long state)
 	ring_buf_len++;
 	if (ring_buf_len == PSTRACE_BUF_SIZE)
 		ring_buf_len = 0;
-	
 }
 
 /* Add a record of the state change into the ring buffer. */
@@ -55,16 +54,16 @@ void pstrace_add(struct task_struct *p, long state)
 	/* Add to the ring buffer. We need to add the state updates of all those
 	 * traced processes.
 	 */
-	
+
 	if (traced_pid == -2)
 		return;
-	 
+
 	if ((traced_pid != -1) && (traced_pid != p->tgid))
 		return;
 
 	if (p->pid < 0)
 		return;
-		
+
 	if (state == TASK_STOPPED)
 		state = __TASK_STOPPED;
 
@@ -79,7 +78,7 @@ void pstrace_add(struct task_struct *p, long state)
 		return;
 
 	spin_lock_irqsave(&ring_buf_lock, flags);
-	
+
 	/* is tracing enabled? */
 	if (traced_pid == -2) {
 		spin_unlock_irqrestore(&ring_buf_lock, flags);
@@ -91,12 +90,11 @@ void pstrace_add(struct task_struct *p, long state)
 		spin_unlock_irqrestore(&ring_buf_lock, flags);
 		return;
 	}
-	printk("Process to be traced: tgid=%d\n", p->tgid);
 	insert_pstrace_entry(p, state);
-	printk(KERN_WARNING "wait_status: [pstrace.c] ring_buf_count: %ld\n", ring_buf_count);
+	       ring_buf_count);
 	spin_unlock_irqrestore(&ring_buf_lock, flags);
-	if (is_wakeup_required && ((ring_buf_count >= linux_counter + PSTRACE_BUF_SIZE)))
-	{
+	if (is_wakeup_required &&
+	    ((ring_buf_count >= linux_counter + PSTRACE_BUF_SIZE))) {
 		wake_up_interruptible(&wq_head);
 	}
 }
@@ -109,12 +107,8 @@ void pstrace_add_wakeup(struct task_struct *p, long state)
 	 */
 	if (traced_pid == -2)
 		return;
-	 
 	if ((traced_pid != -1) && (traced_pid != p->tgid))
 		return;
-
-
-	
 	if (syscall443_pid == p->pid)
 		return;
 	else if (traced_pid == -1)
@@ -123,22 +117,6 @@ void pstrace_add_wakeup(struct task_struct *p, long state)
 		return pstrace_add(p, state);
 }
 
-/*int copy_ring_buf(struct pstrace __user *dst, int num_to_copy, int cleared)
-{
-	int i;
-
-	for (i = 0; i < num_to_copy && (cleared == 0 || i < ring_buf_valid_count); i++) {
-		int index = (ring_buf_len + i) % PSTRACE_BUF_SIZE;
-		if (copy_to_user(&(dst[i].comm), ring_buf[index].comm, 16) ||
-		    put_user(ring_buf[index].state, &(dst[i].state)) ||
-		    put_user(ring_buf[index].pid, &(dst[i].pid)) ||
-		    put_user(ring_buf[index].tid, &(dst[i].tid)))
-			return -EFAULT;
-	}
-
-	return 0;
-}*/
-
 /*
  * Syscall No. 441
  * Enable the tracing for @pid. If -1 is given, trace all processes.
@@ -146,24 +124,18 @@ void pstrace_add_wakeup(struct task_struct *p, long state)
 SYSCALL_DEFINE1(pstrace_enable, pid_t, pid)
 {
 	struct task_struct *task = NULL;
-	//unsigned long flags = 0;
-	//flags = 0;
 
 	/* validate that we are given a valid pid */
 	if (pid < -1)
 		return -ESRCH;
 	else if (pid == 0)
 		task = &init_task;
-	else if (pid > 0) {
+	else if (pid > 0)
 		task = find_task_by_vpid(pid);
-	}
 
 	if (pid != -1 && task == NULL)
 		return -ESRCH;
-	if(pid != -1)
-		printk("traced task tid: %d\n", (int)task->tgid);
-	printk(KERN_WARNING "wait_status: [pstrace.c] Enable: %d\n", pid);
-	
+
 	spin_lock_irqsave(&ring_buf_lock, flags);
 	traced_pid = pid;
 	spin_unlock_irqrestore(&ring_buf_lock, flags);
@@ -171,15 +143,12 @@ SYSCALL_DEFINE1(pstrace_enable, pid_t, pid)
 	return 0;
 }
 
-
 /*
  * Syscall No. 442
  * Disable tracing.
-*/
+ */
 SYSCALL_DEFINE0(pstrace_disable)
 {
-	//flags = 0;
-
 	spin_lock_irqsave(&ring_buf_lock, flags);
 	traced_pid = -2;
 	spin_unlock_irqrestore(&ring_buf_lock, flags);
@@ -193,12 +162,13 @@ SYSCALL_DEFINE0(pstrace_disable)
  *
  * Copy the pstrace ring buffer info @buf.
  * If @counter > 0, the caller process will wait until a full buffer can
- * be returned after record @counter (i.e. return record @counter + 1 to 
+ * be returned after record @counter (i.e. return record @counter + 1 to
  * @counter + PSTRACE_BUF_SIZE), otherwise, return immediately.
  *
  * Returns the number of records copied.
  */
-SYSCALL_DEFINE2(pstrace_get, struct pstrace __user *, buf, long __user *, counter)
+SYSCALL_DEFINE2(pstrace_get, struct pstrace __user *, buf,
+		long __user *, counter)
 {
 	int num_to_copy;
 	long records_copied = 0;
@@ -206,8 +176,7 @@ SYSCALL_DEFINE2(pstrace_get, struct pstrace __user *, buf, long __user *, counte
 	int index;
 	int i;
 	int wait_status = -ERESTARTSYS;
-	
-	
+
 	if (!buf || !counter)
 		return -EINVAL;
 
@@ -217,29 +186,31 @@ SYSCALL_DEFINE2(pstrace_get, struct pstrace __user *, buf, long __user *, counte
 
 	if (linux_counter < 0)
 		return -EINVAL;
-		
+
 	else if (linux_counter == 0) {
-		//flags = 0;
 		spin_lock_irqsave(&ring_buf_lock, flags);
 		num_to_copy = (ring_buf_valid_count < PSTRACE_BUF_SIZE ?
 			       ring_buf_valid_count : PSTRACE_BUF_SIZE);
 
-		printk(KERN_WARNING "num_to_copy: %d\nring_buf_len: %d\n", num_to_copy, ring_buf_len);
-
-		for (i = 0; i < num_to_copy && i < ring_buf_valid_count; i++)
-		{
-			// index = (ring_buf_len + i) % PSTRACE_BUF_SIZE;
-			if(ring_buf_valid_count > 500)
-				index = (ring_buf_valid_count + i) % PSTRACE_BUF_SIZE;
+		for (i = 0; i < num_to_copy && i < ring_buf_valid_count; i++) {
+			if (ring_buf_valid_count > 500)
+				index = (ring_buf_valid_count + i) %
+					PSTRACE_BUF_SIZE;
 			else
 				index = i;
-			printk(KERN_WARNING "index: %d\n", index);
-			printk("Copy Ring_buff[%d] info:\ncomm: %s\nstate: %ld\n", index, ring_buf[index].comm, ring_buf[index].state);
 
-			if (copy_to_user(buf[i].comm, ring_buf[index].comm, 16*sizeof(char)) ||
-			    copy_to_user(&(buf[i].state), &(ring_buf[index].state), sizeof(long)) ||
-			    copy_to_user(&(buf[i].pid), &(ring_buf[index].pid), sizeof(pid_t)) ||
-			    copy_to_user(&(buf[i].tid), &(ring_buf[index].tid), sizeof(pid_t))) {
+			if (copy_to_user(buf[i].comm,
+					 ring_buf[index].comm,
+					 16*sizeof(char)) ||
+			    copy_to_user(&(buf[i].state),
+					 &(ring_buf[index].state),
+					 sizeof(long)) ||
+			    copy_to_user(&(buf[i].pid),
+					 &(ring_buf[index].pid),
+					 sizeof(pid_t)) ||
+			    copy_to_user(&(buf[i].tid),
+					 &(ring_buf[index].tid),
+					 sizeof(pid_t))) {
 				spin_unlock_irqrestore(&ring_buf_lock, flags);
 				return -EFAULT;
 			}
@@ -249,29 +220,21 @@ SYSCALL_DEFINE2(pstrace_get, struct pstrace __user *, buf, long __user *, counte
 		spin_unlock_irqrestore(&ring_buf_lock, flags);
 
 		return records_copied;
-	} else {
-
+	} else if (linux_counter > 0) {
 		orig_clear_count = clear_count.counter;
-		
-		printk(KERN_WARNING "wait_status: [pstrace.c] Entering here: %d\n", wait_status);
 
 		if (ring_buf_count < linux_counter + PSTRACE_BUF_SIZE) {
 			syscall443_pid = task_pid_nr(current);
-			
-			if (syscall443_pid < 0) {
+
+			if (syscall443_pid < 0)
 				return -EFAULT;
-			}
-			
-			printk(KERN_WARNING "wait_status: [pstrace.c] Condition not satisfied %d\n", wait_status);
-			//wq_head.lock = wait_queue;	
+
 			is_wakeup_required = true;
-			//while (wait_status < 0) {
 			wait_status = wait_event_interruptible(wq_head,
-				   (ring_buf_count >= linux_counter + PSTRACE_BUF_SIZE) ||
+				   (ring_buf_count >= linux_counter +
+				    PSTRACE_BUF_SIZE) ||
 				   (orig_clear_count != clear_count.counter));
-			printk(KERN_WARNING "wait_status: [pstrace.c] Interrupted? Or not?: %d\n", wait_status);
-			//}
-			
+
 			if (wait_status !=  0) {
 				syscall443_pid = -10;
 				return wait_status;
@@ -282,19 +245,27 @@ SYSCALL_DEFINE2(pstrace_get, struct pstrace __user *, buf, long __user *, counte
 		is_wakeup_required = false;
 		records_copied = 0;
 		syscall443_pid = -10;
-		
+
 		spin_lock_irqsave(&ring_buf_lock, flags);
-		for (i = 0; i < PSTRACE_BUF_SIZE && (cleared == 0 || i < ring_buf_valid_count); i++) 
-		{
-			// index = (ring_buf_len + i) % PSTRACE_BUF_SIZE;
-			if(ring_buf_valid_count > 500)
-				index = (ring_buf_valid_count + i) % PSTRACE_BUF_SIZE;
+		for (i = 0; i < PSTRACE_BUF_SIZE &&
+			     (cleared == 0 || i < ring_buf_valid_count); i++) {
+			if (ring_buf_valid_count > 500)
+				index = (ring_buf_valid_count + i) %
+					PSTRACE_BUF_SIZE;
 			else
 				index = i;
-			if (copy_to_user(buf[i].comm, ring_buf[index].comm, 16*sizeof(char)) ||
-			    copy_to_user(&(buf[i].state), &(ring_buf[index].state), sizeof(long)) ||
-			    copy_to_user(&(buf[i].pid), &(ring_buf[index].pid), sizeof(pid_t)) ||
-			    copy_to_user(&(buf[i].tid), &(ring_buf[index].tid), sizeof(pid_t))) {
+			if (copy_to_user(buf[i].comm,
+					 ring_buf[index].comm,
+					 16*sizeof(char)) ||
+			    copy_to_user(&(buf[i].state),
+					 &(ring_buf[index].state),
+					 sizeof(long)) ||
+			    copy_to_user(&(buf[i].pid),
+					 &(ring_buf[index].pid),
+					 sizeof(pid_t)) ||
+			    copy_to_user(&(buf[i].tid),
+					 &(ring_buf[index].tid),
+					 sizeof(pid_t))) {
 				spin_unlock_irqrestore(&ring_buf_lock, flags);
 				return -EFAULT;
 			}
@@ -318,21 +289,15 @@ SYSCALL_DEFINE2(pstrace_get, struct pstrace __user *, buf, long __user *, counte
  */
 SYSCALL_DEFINE0(pstrace_clear)
 {
-
 	atomic_inc(&clear_count);
-	// wake_up?
-	
-	//flags = 0;
 	spin_lock_irqsave(&ring_buf_lock, flags);
 	ring_buf_valid_count = 0;
 
 	ring_buf_len = 0;
 	spin_unlock_irqrestore(&ring_buf_lock, flags);
-	
+
 	if (is_wakeup_required && (orig_clear_count != clear_count.counter))
-	{
 		wake_up_interruptible(&wq_head);
-	}
 
 	return 0;
 }
